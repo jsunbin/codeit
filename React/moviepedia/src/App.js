f05efa0970d28ae22fe235541ddc9ff67f5396aa
jsunbin <jsnb6n@gmail.com>
2023-07-07 22:52:57 +0900

    [React] Movie Pedia - 페이지가 처음 렌더링 될 때 데이터를 불러오도록 수정

--- React/moviepedia/src/App.js
+++ React/moviepedia/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getReviews } from "./API/API";
 import ReviewList from "./components/ReviewList";
 
@@ -19,10 +19,15 @@ function App() {
     setItems(nextItems)
   };
 
-  const handleLoadClick = async () => {
+  const handleLoad = async () => {
     const { reviews } = await getReviews()
     setItems(reviews)
   }
+
+  useEffect(() => {
+    handleLoad()
+  }, [])
+  
 
   return (
     <>
@@ -31,7 +36,6 @@ function App() {
       <button onClick={handleBestClick}>베스트순</button>
     </div>
     <ReviewList items={sortedItems} onDelete={handleDelete}/>
-    <button onClick={handleLoadClick}>불러오기</button>
     </>
   );
 }
