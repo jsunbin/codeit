fb258e25506aec85ab793c34227f85090b817c79
jsunbin <jsnb6n@gmail.com>
2023-07-07 22:47:27 +0900

    [React] Movie Pedia - API 음식 목록 가져오기

--- React/moviepedia/src/App.js
+++ React/moviepedia/src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
+import { getReviews } from "./API/API";
 import ReviewList from "./components/ReviewList";
-import mockItems from "./data/mock.json";
 
 function App() {
-  const [items, setitems] = useState(mockItems)
+  const [items, setItems] = useState([])
   const [order, setOrder] = useState('createdAt');
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -16,7 +16,12 @@ function App() {
 
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
-    setitems(nextItems)
+    setItems(nextItems)
+  };
+
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews()
+    setItems(reviews)
   }
 
   return (
@@ -26,6 +31,7 @@ function App() {
       <button onClick={handleBestClick}>베스트순</button>
     </div>
     <ReviewList items={sortedItems} onDelete={handleDelete}/>
+    <button onClick={handleLoadClick}>불러오기</button>
     </>
   );
 }
