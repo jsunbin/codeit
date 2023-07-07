5fad8762ac6967bbf55fb1565fb2633846c4064a
jsunbin <jsnb6n@gmail.com>
2023-07-07 22:28:48 +0900

    [React] Movie Pedia - li에 key 추가

--- React/moviepedia/src/components/ReviewList.jsx
+++ React/moviepedia/src/components/ReviewList.jsx
@@ -29,7 +29,7 @@ export default function ReviewList({ items, onDelete }) {
     <ul>
       {items.map((item) => {
         return (
-          <li>
+          <li key={item.id}>
             <ReviewListItem item={item} onDelete={onDelete} />
           </li>
         );
