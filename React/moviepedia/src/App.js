bcce465c1bd72726ec6097e2a54feada4a886d6c
jsunbin <jsnb6n@gmail.com>
2023-07-07 21:23:53 +0900

    React] Movie Pedia - 아이템 삭제 기능 추가

--- React/moviepedia/src/App.js
+++ React/moviepedia/src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import ReviewList from "./components/ReviewList";
-import items from "./data/mock.json";
+import mockItems from "./data/mock.json";
 
 function App() {
+  const [items, setitems] = useState(mockItems)
   const [order, setOrder] = useState('createdAt');
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -13,13 +14,18 @@ function App() {
     setOrder('rating')
   }
 
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setitems(nextItems)
+  }
+
   return (
     <>
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>베스트순</button>
     </div>
-    <ReviewList items={sortedItems}/>
+    <ReviewList items={sortedItems} onDelete={handleDelete}/>
     </>
   );
 }
