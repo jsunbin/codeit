baa4571890f93873c6ca04890e07fd6bbc2d761e
jsunbin <jsnb6n@gmail.com>
2023-07-09 22:45:43 +0900

    [React] Movie Pedia - 네트워크 로딩, 에러 처리 코드 추가

--- React/moviepedia/src/API/API.js
+++ React/moviepedia/src/API/API.js
@@ -1,6 +1,11 @@
-export async function getReviews({order="createdAt", offset=0, limit=6}) {
+export async function getReviews({ order="createdAt", offset=0, limit=6 }) {
   const query = `order=${order}&offset=${offset}&limit=${limit}`
   const response = await fetch(`https://learn.codeit.kr/api/film-reviews?${query}`);
+
+  if(!response.ok) {
+    throw new Error('네트워크에 오류가 발생했습니다!')
+  }
+
   const data = await response.json();
   return data;
 }
--- React/moviepedia/src/App.js
+++ React/moviepedia/src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [order, setOrder] = useState('createdAt');
   const [offset, setOffset] = useState(0)
   const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -25,7 +27,19 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    const { reviews, paging } = await getReviews(options);
+    let result;
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;   
+    } finally {
+      setIsLoading(false)
+    }
+    const { reviews, paging } = result;
+
     if (options.offset === 0) {
       setItems(reviews)
       
@@ -53,7 +67,8 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete}/>
-      {hasNext && <button onClick={handleLoadMore}>더보기</button>}
+      {hasNext && <button disabled={isLoading} onClick={handleLoadMore}>더보기</button>}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </>
   );
 }
