c6fe63285e410a1ff385eb730651cb01ab02a8d1
jsunbin <jsnb6n@gmail.com>
2023-09-12 23:58:40 +0900

    [React] Foodit - 파일 인풋 기능 추가

--- React/foodit/src/components/FoodForm.jsx
+++ React/foodit/src/components/FoodForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FileInput from "./FileInput";
 import "./FoodForm.css";
 
 export default function FoodForm() {
@@ -6,17 +7,19 @@ export default function FoodForm() {
     title: "",
     calorie: 0,
     content: "",
+    imgFile: null,
   });
-  const [title, setTitle] = useState("");
-  const [calorie, setCalorie] = useState(0);
-  const [content, setContent] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (name, value) => {
     setValues((prev) => ({
       ...prev,
       [name]: value,
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
   };
 
   const handleSubmit = (e) => {
@@ -26,9 +29,10 @@ export default function FoodForm() {
 
   return (
     <form className="FoodForm" onSubmit={handleSubmit}>
-      <input name="title" value={values.title} onChange={handleChange} />
-      <input name="calorie" value={values.calorie} type="number" onChange={handleChange} />
-      <textarea name="content" value={values.content} onChange={handleChange} />
+      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <input name="calorie" value={values.calorie} type="number" onChange={handleInputChange} />
+      <textarea name="content" value={values.content} onChange={handleInputChange} />
       <button type="submit">확인</button>
     </form>
   );
