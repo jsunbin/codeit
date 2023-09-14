6e1e9c2d62c7ac0ebe488c5038a13422009e5ba2
jsunbin <jsnb6n@gmail.com>
2023-09-14 23:20:15 +0900

    [React] Movie Pedia - ref 관련 주석 삭제

--- React/moviepedia/src/components/FileInput.jsx
+++ React/moviepedia/src/components/FileInput.jsx
@@ -9,9 +9,6 @@ export default function FileInput({ name, value, onChange }) {
   };
 
   const handleClearClick = () => {
-    // DOM 노드는 반드시 렌더링이 끝나야 생기므로, ref객체도 화면에 컴포넌트가 렌더링 되었을 대만 존재함
-    // 조건부 렌더링으로 인해서 이 값이 없을 수도 있므
-    // 따라서, 항상, 아래처럼 ref 객체의 current 값이 존재하는 지 확인하고 사용하는 것을 추천
     const inputNode = inputRef.current;
 
     if (!inputNode) return;
