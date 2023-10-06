d7b84992dd7025b148376bb7d469476f9be81940
jsunbin <jsnb6n@gmail.com>
2023-10-06 17:39:00 +0900

    [React] codethat - 중첩 Route, Outlet을 활용하여 하위 페이지 나누기

--- React/codethat/src/Main.js
+++ React/codethat/src/Main.js
@@ -10,19 +10,24 @@ import QuestionPage from './pages/QuestionPage';
 function Main() {
   return (
     <BrowserRouter>
-      <App>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="courses" element={<CourseListPage />} />
-          <Route
-            path="courses/react-frontend-development"
-            element={<CoursePage />}
-          />
-          <Route path="wishlist" element={<WishlistPage />} />
-          <Route path="questions" element={<QuestionListPage />} />
-          <Route path="/questions/616825" element={<QuestionPage />} />
+          <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
+            <Route path="courses">
+              <Route index element={<CourseListPage />} />
+              <Route
+                path="react-frontend-development"
+                element={<CoursePage />}
+              />
+            </Route>
+            <Route path="questions">
+              <Route index element={<QuestionListPage />} />
+              <Route path="616825" element={<QuestionPage />} />
+            </Route>
+            <Route path="wishlist" element={<WishlistPage />} />
+          </Route>
+
         </Routes>
-      </App>
     </BrowserRouter>
   );
 }
---
+++ React/codethat/src/components/App.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import styles from "./App.module.css";
+import "./App.font.css";
+
+function App() {
+  return (
+    <>
+      <Nav className={styles.nav} />
+      <div className={styles.body}>
+        <Outlet />
+      </div>
+      <Footer className={styles.footer} />
+    </>
+  );
+}
+
+export default App;
