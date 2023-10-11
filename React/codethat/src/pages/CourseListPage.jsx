bbe44ed9e5dae848e2497e6132b55cb0a4021fa0
jsunbin <jsnb6n@gmail.com>
2023-10-11 16:55:19 +0900

    [React] codethat - useSearchParams로 쿼리 사용하여 검색 기능 구현

--- React/codethat/src/pages/CourseListPage.jsx
+++ React/codethat/src/pages/CourseListPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import ListPage from "../components/ListPage";
 import Warn from "../components/Warn";
 import CourseItem from "../components/CourseItem";
@@ -8,14 +9,21 @@ import searchBarStyles from "../components/SearchBar.module.css";
 import searchIcon from "../assets/search.svg";
 
 function CourseListPage() {
-  const [keyword, setKeyword] = useState("");
-  const courses = getCourses();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initKeyword = searchParams.get("keyword");
+  const [keyword, setKeyword] = useState(initKeyword || "");
+  const courses = getCourses(initKeyword);
 
   const handleKeywordChange = (e) => setKeyword(e.target.value);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchParams(keyword ? { keyword } : {});
+  };
+
   return (
     <ListPage variant="catalog" title="모든 코스" description="자체 제작된 코스들로 기초를 쌓으세요.">
-      <form className={searchBarStyles.form}>
+      <form className={searchBarStyles.form} onSubmit={handleSubmit}>
         <input name="keyword" value={keyword} onChange={handleKeywordChange} placeholder="검색으로 코스 찾기"></input>
         <button type="submit">
           <img src={searchIcon} alt="검색" />
@@ -24,7 +32,7 @@ function CourseListPage() {
 
       <p className={styles.count}>총 {courses.length}개 코스</p>
 
-      {courses.length === 0 ? (
+      {initKeyword && courses.length === 0 ? (
         <Warn
           className={styles.emptyList}
           title="조건에 맞는 코스가 없어요."
