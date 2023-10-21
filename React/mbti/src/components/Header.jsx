0c50929309157e04c1801a049b66c83e6dfd89ba
jsunbin <jsnb6n@gmail.com>
2023-10-21 23:58:33 +0900

    [React] MBTI - 헤더 컴포넌트 마크업

---
+++ React/mbti/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        <div>MBTI별</div>
+        <strong>좋아하는 컬러</strong>
+      </h1>
+    </header>
+  );
+}
