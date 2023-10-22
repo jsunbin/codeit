a6ab608000585b5fe940fe0758fd7df55e838c15
jsunbin <jsnb6n@gmail.com>
2023-10-22 23:52:51 +0900

    [React] MBTI - Routes로 페이지 나누기

--- React/mbti/src/App.js
+++ React/mbti/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<HomePage/>}/>
+            <Route path='new' element={<HomePage/>}/>
+          </Route>
+        </Routes>
+
+    </BrowserRouter>
   );
 }
 export default App;
