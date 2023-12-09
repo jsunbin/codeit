0c7756398e1802a3e5027be1c77227f476006493
jsunbin <jsnb6n@gmail.com>
2023-12-09 19:07:40 +0900

    [TypeScript] 코드잇 게임즈 - 배열과 튜플을 사용하여 변수에 맞는 타입 지정

--- TypeScript/codeitGames/main.ts
+++ TypeScript/codeitGames/main.ts
@@ -1,3 +1,5 @@
+// 1. 변수 값에 맞는 타입 지정
+
 let monsterName: string;
 let level: number;
 let hasGold: boolean;
@@ -16,3 +18,16 @@ console.log(
     `${skill ? `가진 능력은 ${skill} 이고 \n` : ""}` +
     `${area ? `출현 지역은 ${area} 입니다.` : ""}`
 );
+
+// 2.주어진 코드에 맞게, 배열과 튜플 타입을 사용해서 current, target, 그리고 items 변수에 명시적으로 타입을 지정해 주세요.
+const current: [number, number] = [0, 0];
+const target: [number, number] = [4, 5];
+
+const dx = target[0] - current[0];
+const dy = target[1] - current[1];
+console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+
+const items: string[] = [];
+items.push("갑옷");
+items.push("빨간 물약");
+console.log(`${items.join(", ")}을/를 획득했다!`);
