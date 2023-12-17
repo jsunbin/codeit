1653f6a49acce3d6458c47cca7f9fa5d6f654b2d
jsunbin <jsnb6n@gmail.com>
2023-12-17 23:35:07 +0900

    [TypeScript] 리터럴 타입

--- TypeScript/basics/main.js
+++ TypeScript/basics/main.js
@@ -1,14 +1,12 @@
+"use strict";
 // 리터럴 타입 ===============
 // 값을 자체를 타입으로 하는 타입, const로 선언된 변수가 보통 이에 해당
 let productName1 = "코드잇 블랙 후드";
 const productName2 = "코드잇 텀블러";
-
 let small = 95;
 const large = 100;
-
-function printSize(size: number) {
-  console.log(`${size} 사이즈입니다.`);
+function printSize(size) {
+    console.log(`${size} 사이즈입니다.`);
 }
-
 printSize(small);
 printSize(large);
