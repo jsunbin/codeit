f70f0bdcc1227ff54c73029c63ddae3c153d4d33
jsunbin <jsnb6n@gmail.com>
2023-12-20 22:49:17 +0900

    [TypeScript] 타입 별칭 실습 - 반복되는 코드 줄이기

--- TypeScript/basics/main.js
+++ TypeScript/basics/main.js
@@ -16,3 +16,23 @@ const user = {
     email: "typescript@codeit.kr",
     cart,
 };
+function getDiff(fromPoint, toPoint) {
+    let dx = toPoint[0] - fromPoint[0];
+    let dy = toPoint[1] - fromPoint[1];
+    return [dx, dy];
+}
+const monster = {
+    id: "g001",
+    name: "고블린",
+    level: 22,
+    skills: ["태권도", "특공무술"],
+    move(fromPoint, toPoint) {
+        let [dx, dy] = getDiff(fromPoint, toPoint);
+        console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+};
+const current = [0, 0];
+const target = [4, 5];
+monster.move(current, target);
--- TypeScript/basics/main.ts
+++ TypeScript/basics/main.ts
@@ -38,3 +38,44 @@ const user: User = {
   email: "typescript@codeit.kr",
   cart,
 };
+
+// 실습: 반복되는 코드 줄이기 ===============
+// [number, number]라는 튜플의 반복을 줄이고 재사용성이 높은 코드로 바꾸자. 타입 별칭을 이용해서 Point라는 타입으로 만들어 보세요.
+type Point = [number, number];
+
+interface Entity {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Monster extends Entity {
+  name: string;
+  level: number;
+  hasGold?: boolean;
+  skills: string[];
+  move: (fromPoint: Point, toPoint: Point) => void;
+}
+
+function getDiff(fromPoint: Point, toPoint: Point) {
+  let dx = toPoint[0] - fromPoint[0];
+  let dy = toPoint[1] - fromPoint[1];
+  return [dx, dy];
+}
+
+const monster: Monster = {
+  id: "g001",
+  name: "고블린",
+  level: 22,
+  skills: ["태권도", "특공무술"],
+  move(fromPoint, toPoint) {
+    let [dx, dy] = getDiff(fromPoint, toPoint);
+    console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+  },
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+const current: Point = [0, 0];
+const target: Point = [4, 5];
+monster.move(current, target);
