c9e44766e7fea79c2a421126c1c44725dbbe2fbb
jsunbin <jsnb6n@gmail.com>
2023-12-24 23:58:18 +0900

    [TypeScript] 코드잇 게임즈 - 아이템 정보 출력하기 (keyof 와 typeof)

--- TypeScript/codeitGames/main.js
+++ TypeScript/codeitGames/main.js
@@ -109,3 +109,17 @@ const item1Intersection = {
     defence: 42,
 };
 printEquipmentIntersection(item1Intersection);
+const itemTable = [
+    ["name", "이름"],
+    ["price", "가격"],
+    ["description", "설명"],
+];
+const item = {
+    id: "h001",
+    name: "힐링 포션",
+    price: 200,
+    description: "마시면 체력을 50 회복한다.",
+};
+for (let [propertyKey, propertyName] of itemTable) {
+    console.log(`${propertyName} | ${item[propertyKey]}`);
+}
--- TypeScript/codeitGames/main.ts
+++ TypeScript/codeitGames/main.ts
@@ -197,3 +197,28 @@ const item1Intersection: WeaponIntersection & ArmorIntersection = {
 };
 
 printEquipmentIntersection(item1Intersection);
+
+// keyof 와 typeof: 아이템 정보 출력하기
+interface Item {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+}
+
+const itemTable: [keyof Item, string][] = [
+  ["name", "이름"],
+  ["price", "가격"],
+  ["description", "설명"],
+];
+
+const item: Item = {
+  id: "h001",
+  name: "힐링 포션",
+  price: 200,
+  description: "마시면 체력을 50 회복한다.",
+};
+
+for (let [propertyKey, propertyName] of itemTable) {
+  console.log(`${propertyName} | ${item[propertyKey]}`);
+}
