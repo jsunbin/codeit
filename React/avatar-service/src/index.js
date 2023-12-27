f74a28390d51b0d64ae63990a89443a5f73a5f10
jsunbin <jsnb6n@gmail.com>
2023-12-28 00:34:06 +0900

    [React] Avatar - 프로젝트 수정

--- React/avatar-service/src/index.js
+++ React/avatar-service/src/index.js
@@ -1,5 +1,9 @@
-import ReactDOM from 'react-dom';
+import React from "react";
+// import ReactDOM from 'react-dom'; //구버전
+import { createRoot } from "react-dom/client";
 import Main from './Main';
 import './index.css';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Main />);
